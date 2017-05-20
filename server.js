8ea201d498b8b8fb9302f255ad0df872ab1588c3
Toni Räsänen <toni.rasanen@iki.fi>
2017-05-20 16:16:14 +0300

    api documentation with Swagger

--- server.js
+++ server.js
@@ -2,28 +2,50 @@
 
 const Hapi = require('hapi');
 const Good = require('good');
-const Joi  = require('joi');
-
-
+const Joi = require('joi');
+const Inert = require('inert');
+const Vision = require('vision');
+const Mongoose = require('mongoose');
+const Swagger = require('hapi-swagger');
 const server = new Hapi.Server();
-server.connection({ port: (process.env.PORT || 3000), host: '0.0.0.0' });
 
+server.connection({
+    port: (process.env.PORT || 3000),
+    host: '0.0.0.0'
+});
 
 server.route({
     method: 'GET',
     path: '/',
-    handler: function (request, reply) {
+    handler: function(request, reply) {
         reply('Hello, world!');
     }
 });
 
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function(request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    },
+    config: {
+        validate: {
+            params: {
+                name: Joi.required()
+            }
+        },
+        description: 'Just testing, say hello!',
+        notes: 'Just for testing, passing name as a request param',
+        tags: ['api', 'hello']
+    }
+});
 
 server.route({
     method: 'GET',
     path: '/observations/{year}',
-    handler: function (request, reply) {
+    handler: function(request, reply) {
         reply('[{name: "Kangaskiuru", year: ' + encodeURIComponent(request.params.year) +
-              ', location: {lat: 23.34322, lng: 32.3456}}]');
+            ', location: {lat: 23.34322, lng: 32.3456}}]');
     },
     config: {
         validate: {
@@ -37,31 +59,35 @@ server.route({
     }
 });
 
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
-
-server.register({
-    register: Good,
-    options: {
-        reporters: {
-            console: [{
-                module: 'good-squeeze',
-                name: 'Squeeze',
-                args: [{
-                    response: '*',
-                    log: '*'
-                }]
-            }, {
-                module: 'good-console'
-            }, 'stdout']
+server.register([
+    Inert,
+    Vision,
+    {
+        register: Swagger,
+        options: {
+            info: {
+                title: 'Test API Documentation'
+            }
+        }
+    },
+    {
+        register: Good,
+        options: {
+            reporters: {
+                console: [{
+                    module: 'good-squeeze',
+                    name: 'Squeeze',
+                    args: [{
+                        response: '*',
+                        log: '*'
+                    }]
+                }, {
+                    module: 'good-console'
+                }, 'stdout']
+            }
         }
     }
-}, (err) => {
+], (err) => {
 
     if (err) {
         throw err; // something bad happened loading the plugin
