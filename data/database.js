c721d8fd45f8450e3cd79170f6626301800e59f8
Toni Räsänen <toni.rasanen@iki.fi>
2017-05-24 20:53:02 +0300

    configure mongoose to use ES6 Promises

--- data/database.js
+++ data/database.js
@@ -7,6 +7,9 @@ Mongoose.connect(process.env.MONGO_URI);
 
 const db = Mongoose.connection;
 
+// Use native ES6 promises (see http://mongoosejs.com/docs/promises.html)
+Mongoose.Promise = global.Promise;
+
 db.on('error', console.error.bind(console, 'connection error'));
 
 db.once('open', function callback() {
